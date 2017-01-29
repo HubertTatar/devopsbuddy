61c655aa0e77528767c84891899245b5bf67eff7
HubertTatar <hutatar@gmail.com>
2017-01-29 22:35:42 +0100

    added index

--- src/main/java/com/devops/web/controllers/IndexController.java
+++ src/main/java/com/devops/web/controllers/IndexController.java
@@ -1,7 +1,13 @@
 package com.devops.web.controllers;
 
-/**
- * Created by huta on 29.01.2017.
- */
-public class IndexCOntroller {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "index";
+    }
 }
